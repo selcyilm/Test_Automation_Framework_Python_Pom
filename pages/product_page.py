6548e018457ee6361a6da9be09a8a6978a76db28
Selcuk <selcukemnn@gmail.com>
2025-03-14 19:28:12 +0300

    chore: delete non-used methods

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,18 +8,9 @@ from pages.base_page import BasePage
 class ProductPage(BasePage):
     PRODUCT_TITLE = (By.CSS_SELECTOR, "span#productTitle")
     ADD_TO_CART_BUTTON = (By.ID, "add-to-cart-button")
-    CART_COUNT_NUMBER = (By.ID, "nav-cart-count")
-    CART_PAGE_ICON = (By.ID, "nav-cart")
 
     def add_product_to_card(self):
         self.hover_and_click(*self.ADD_TO_CART_BUTTON)
-
-    def get_how_many_product_on_cart(self):
-        #time.sleep(2)
-        return int(self.get_text(self.CART_COUNT_NUMBER))
-
-    def navigate_to_card_page(self):
-        self.hover_and_click(*self.CART_PAGE_ICON)
 
     def get_product_title(self):
         return self.get(*self.PRODUCT_TITLE)
