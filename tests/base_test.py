a8663901302b7ccde0b041d0d1aba53371f7ba34
Selcuk <selcukemnn@gmail.com>
2025-03-14 19:21:27 +0300

    chore: move driver.get(url) to test file

--- tests/base_test.py
+++ tests/base_test.py
@@ -19,7 +19,6 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         config = self.get_json_config()
         self.driver = DriverFactory.init_driver(config)
-        self.driver.get(config["base_url"])
 
     def tearDown(self):
         self.driver.quit()
