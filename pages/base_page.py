1676ab31b0eac0da14bebae6bec20ecea9e58d21
Selcuk <selcukemnn@gmail.com>
2025-03-15 23:14:47 +0300

    docs: add docs for base page and header comp

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,36 +9,80 @@ class BasePage(object):
         self.wait = WebDriverWait(self.driver, 20)
 
     def get(self, *locator):
+        """
+        Find the web element with using specific locator
+        :param locator: tuple containing locator type and value
+        :return: WebElement, first element its find
+        """
         return self.driver.find_element(*locator)
 
     def get_multiple(self, *locator):
+        """
+        Finds multiple web elements using locator
+        :param locator: tuple containing locator type and value
+        :return: list of WebElements its find
+        """
         return self.driver.find_elements(*locator)
 
     def click_element(self, *by_locator):
+        """
+        Clicks on the element identified by locator
+        :param by_locator: tuple containing locator type and value
+        """
         self.get(*by_locator).click()
 
     def hover(self, *by_locator):
+        """
+        Hovers to specific element identified by locator
+        :param by_locator: tuple containing locator type and value
+        """
         action = ActionChains(self.driver)
         action.move_to_element(self.get(*by_locator)).perform()
 
     def hover_and_click(self, *by_locator):
+        """
+        Hovers and clicks to specific element identified by locator
+        :param by_locator: tuple containing locator type and value
+        """
         element = self.get(*by_locator)
         action = ActionChains(self.driver)
         action.move_to_element(element).perform()
         element.click()
 
     def get_current_url(self):
+        """
+        :return: current url of the page
+        """
         return self.driver.current_url
 
     def get_page_title(self):
+        """
+        :return: title of the page
+        """
         return self.driver.title
 
     def wait_element(self, method, message=''):
+        """
+        Waits to element until it is clickable
+        :param method: Locator tuple for element
+        :param message: Optional message for timeout exception
+        :return: clickable WebElement
+        """
         return self.wait.until(EC.element_to_be_clickable(method), message)
 
     def get_text(self, locator):
+        """
+        Waits and get the text of an element
+        :param locator: Locator tuple for element
+        :return: WebElement's text value
+        """
         return self.wait_element(locator).text
 
     def select_by_visible_text(self, locator, visible_text):
+        """
+        Select option from drop down with visible text
+        :param locator: Locator tuple for element
+        :param visible_text: Option to select
+        """
         select = Select(self.get(*locator))
         select.select_by_visible_text(visible_text)
--- pages/components/header_comp.py
+++ pages/components/header_comp.py
@@ -5,6 +5,10 @@ from pages.base_page import BasePage
 
 
 class HeaderComp(BasePage):
+    """
+    Component class representing Amazon's header navigation bar.
+    Provides methods to interact with common header elements.
+    """
     LOGO_ICON = (By.ID, "nav-logo-sprites")
     LOCATION_LINK = (By.ID, "nav-global-location-popover-link")
     SEARCH_DD_CATEGORY = (By.ID, "searchDropdownBox")
@@ -16,20 +20,45 @@ class HeaderComp(BasePage):
     CART_COUNT_TEXT = (By.ID, "nav-cart-count")
 
     def navigate_to_homepage(self):
+        """
+        Clicks on the Amazon logo to navigate back to homepage.
+        """
         self.get(*self.LOGO_ICON).click()
 
     def click_on_location(self):
+        """
+        Clicks on the location link to open the delivery location dialog.
+        """
         self.click_element(*self.LOCATION_LINK)
 
     def pick_category(self, category_name):
+        """
+        Selects a specific category from the search dropdown.
+
+        :arg category_name: The visible text of the category to select
+        """
         self.select_by_visible_text(self.SEARCH_DD_CATEGORY, category_name)
 
     def search_product(self, product_name):
+        """
+        Searches for a product by entering text and clicking search button.
+
+        :arg product_name: Keywords to search for
+        """
         self.get(*self.SEARCH_BOX_AREA).send_keys(product_name)
         self.click_element(*self.SEARCH_SUBMIT_BUTTON)
 
     def navigate_to_cart_page(self):
+        """
+        Navigates to the shopping cart page by clicking the cart icon.
+        """
         self.click_element(*self.CART_LINK)
 
     def get_number_of_products_on_cart(self):
+        """
+        On the card page icon, there is a number representing how many products
+        are on the cart page, It basically extracts the numerical value displayed
+        on the cart icon
+        :return: Number of items on the shopping cart
+        """
         return int(self.get_text(self.CART_COUNT_TEXT))
