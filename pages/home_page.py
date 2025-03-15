c747dcf700386e014b360f083e9f7e368b6d4384
Selcuk <selcukemnn@gmail.com>
2025-03-15 23:24:20 +0300

    docs: add docs for home page methods

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,4 +8,9 @@ class HomePage(BasePage):
     ACCEPT_COOKIES_BUTTON = (By.ID, "sp-cc-accept")
 
     def accept_cookies(self):
+        """
+        Accept cookies by clicking accept button
+        This method should be called when you access the website first time
+        to handle the cookie consent
+        """
         self.click_element(*self.ACCEPT_COOKIES_BUTTON)
