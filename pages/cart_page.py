81c73ea1baf7c884ee03289582c19c56f22c592d
Selcuk <selcukemnn@gmail.com>
2025-03-15 23:19:31 +0300

    docs: add docs for cart page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -8,7 +8,13 @@ class CardPage(BasePage):
     DELETE_PRODUCT_FROM_CARD_BUTTON = (By.CSS_SELECTOR, "[value='Sil']")
 
     def get_cart_product_full_name(self):
+        """
+        :return: The complete product name of the item in the cart
+        """
         return self.get(*self.CART_PRODUCT_FULL_NAME).get_attribute("textContent")
 
     def delete_product_from_cart(self):
+        """
+        Removes a product from the shopping cart
+        """
         self.hover_and_click(*self.DELETE_PRODUCT_FROM_CARD_BUTTON)
