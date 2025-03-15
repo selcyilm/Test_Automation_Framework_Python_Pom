42b5f2ed091829bee4c82f5d10031081b2820c7f
Selcuk <selcukemnn@gmail.com>
2025-03-15 22:51:26 +0300

    docs: add definition for base test methods

--- tests/base_test.py
+++ tests/base_test.py
@@ -9,16 +9,27 @@ class BaseTest(unittest.TestCase):
     config_path = "../config.json"
 
     def get_json_config(self):
+        """
+        Loads and parses config file
+        :return: json object for specified path
+        """
         config_file = open(self.config_path, "r")
         config = json.load(config_file)
         config_file.close()
         return config
 
     def wait_if_defined(self):
+        """
+        Implements static wait if enabled in the config file.
+        When static-wait.enabled is true it will wait for specified number of seconds.
+        """
         if self.get_json_config()["static-wait"]["enabled"]:
             time.sleep(self.get_json_config()["static-wait"]["wait-time"])
 
     def get_base_url(self):
+        """
+        :return: The base url for test
+        """
         return self.get_json_config()["base_url"]
 
     def setUp(self):
